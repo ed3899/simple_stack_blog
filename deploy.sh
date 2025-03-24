732bdb4d8013cf83deee78d249a738da5298be65
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2025-03-24 14:39:02 +0000

    dokploy last phase

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+curl -X 'POST' \
+  'https://dokploy.simplestack.world/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer simple_stack_bloghHMswNtcNkffyMghSUzHKZaealhbqkjISjGFcjtlUaRSpgjYfqdMCGwvHvvyycpQ' \
+  -d '{
+  "applicationId": "string"
+}'
